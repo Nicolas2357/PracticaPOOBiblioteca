8b462efdb5193aeb779f0549e535fca751977145
Nicolas Ponce Carmona <poncecarmonanicolas09@gmail.com>
2026-06-17 11:17:01 -0600

    Agregar archivos de proyecto.

---
+++ PracticaPOOBiblioteca/Libro.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaPOOBiblioteca
+{
+    internal class Libro
+    {
+        private string titulo;
+        private string autor;
+        private int anio;
+        private bool disponible;
+
+        public Libro(string titulo, string autor, int anio)
+        {
+            this.titulo = titulo;
+            this.autor = autor;
+            this.anio = anio;
+            disponible = true;
+        }
+
+        public void MostrarInfo()
+        {
+            Console.WriteLine("Título: " + titulo);
+            Console.WriteLine("Autor: " + autor);
+            Console.WriteLine("Año: " + anio);
+            Console.WriteLine("Disponible: " + disponible);
+        }
+
+        public void PrestarLibro()
+        {
+            if (disponible)
+            {
+                disponible = false;
+                Console.WriteLine("Libro prestado correctamente.");
+            }
+            else
+            {
+                Console.WriteLine("El libro ya está prestado.");
+            }
+        }
+
+        public void DevolverLibro()
+        {
+            disponible = true;
+            Console.WriteLine("Libro devuelto correctamente.");
+        }
+    }
+}
---
+++ PracticaPOOBiblioteca/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaPOOBiblioteca
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Libro libro1 = new Libro(
+           "Don Quijote de la Mancha",
+           "Miguel de Cervantes",
+           1605);
+
+            libro1.MostrarInfo();
+
+            Console.WriteLine();
+
+            libro1.PrestarLibro();
+
+            Console.WriteLine();
+
+            libro1.MostrarInfo();
+
+            Console.WriteLine();
+
+            libro1.DevolverLibro();
+
+            Console.WriteLine();
+
+            libro1.MostrarInfo();
+        }
+    }
+}
